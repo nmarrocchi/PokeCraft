026aaa72c6e5462564e64e26b17db5de13589a06
nmarrocchi <nicolas.marrocchi@yahoo.com>
2025-03-14 03:00:04 +0100

    First Commit

---
+++ src/main/java/com/Konatsu/pokecraft/pokecraft.java
@@ -0,0 +1,18 @@
+package com.Konatsu.pokecraft;
+
+import net.fabricmc.api.ModInitializer;
+
+public class pokecraft implements ModInitializer {
+    public static final String MOD_ID = "pokecraft";
+
+    @Override
+    public void onInitialize() {
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("╔════════════════════════════════╗");
+        System.out.println("║     POKECRAFT EST CHARGÉ !     ║");
+        System.out.println("╚════════════════════════════════╝");
+        System.out.println(" ");
+        System.out.println(" ");
+    }
+}
